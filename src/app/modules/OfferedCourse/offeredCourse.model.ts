e8599f0747a2e1b1ebbd8d4dcf6c178c3d4164c0
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-06-27 15:46:41 +0600

    Implement some business logic on the create

--- src/app/modules/OfferedCourse/offeredCourse.model.ts
+++ src/app/modules/OfferedCourse/offeredCourse.model.ts
@@ -42,11 +42,13 @@ const offeredCourseSchema = new Schema<TOfferedCourse>(
 			type: Number,
 			required: true,
 		},
-		days: {
-			type: String,
-			enum: Days,
-			required: true,
-		},
+		days: [
+			{
+				type: String,
+				enum: Days,
+				required: true,
+			},
+		],
 		startTime: {
 			type: String,
 			required: true,
--- src/app/modules/OfferedCourse/offeredCourse.service.ts
+++ src/app/modules/OfferedCourse/offeredCourse.service.ts
@@ -1,8 +1,64 @@
+import status from 'http-status';
+import AppError from '../../errors/AppError';
+import { SemesterRegistration } from '../semesterRegistration/semesterRegistration.model';
 import { TOfferedCourse } from './offeredCourse.interface';
 import { OfferedCourse } from './offeredCourse.model';
+import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
+import { AcademicDepartment } from '../academicDepartment/academicDepartment.model';
+import { Course } from '../Course/course.model';
+import { Faculty } from '../faculty/faculty.model';
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
-	const result = await OfferedCourse.create(payload);
+	const {
+		semesterRegistration,
+		academicFaculty,
+		academicDepartment,
+		course,
+		faculty,
+	} = payload;
+
+	// check if the semesterRegistration id is exists!
+	const isSemesterRegistrationExist =
+		await SemesterRegistration.findById(semesterRegistration);
+
+	if (!isSemesterRegistrationExist) {
+		throw new AppError(status.NOT_FOUND, 'Semester Registration not found');
+	}
+
+	// check if the AcademicFaculty id is exists!
+	const isAcademicFacultyExist =
+		await AcademicFaculty.findById(academicFaculty);
+
+	if (!isAcademicFacultyExist) {
+		throw new AppError(status.NOT_FOUND, 'Semester Faculty not found');
+	}
+
+	// check if the AcademicDepartment id is exists!
+	const isAcademicDepartmentExist =
+		await AcademicDepartment.findById(academicDepartment);
+
+	if (!isAcademicDepartmentExist) {
+		throw new AppError(status.NOT_FOUND, 'Semester Department not found');
+	}
+
+	// check if the Course id is exists!
+	const isCourseExist = await Course.findById(course);
+
+	if (!isCourseExist) {
+		throw new AppError(status.NOT_FOUND, 'Course not found');
+	}
+
+	// check if the Faculty id is exists!
+	const isFacultyExist = await Faculty.findById(faculty);
+
+	if (!isFacultyExist) {
+		throw new AppError(status.NOT_FOUND, 'Faculty not found');
+	}
+
+	const academicSemester = isSemesterRegistrationExist.academicSemester;
+
+	const result = await OfferedCourse.create({ ...payload, academicSemester });
+
 	return result;
 };
 
