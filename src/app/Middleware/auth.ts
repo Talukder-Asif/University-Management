497471f67cd80cd73c720a980c49923441670a73
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-07-01 20:52:01 +0600

    authorization by role done in creating student

--- src/app/Middleware/auth.ts
+++ src/app/Middleware/auth.ts
@@ -1,10 +1,11 @@
+import { TUserRole } from './../modules/user/user.interface';
 import status from 'http-status';
 import AppError from '../errors/AppError';
 import catchAsync from '../utils/CatchAsync';
 import { NextFunction, Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
 	return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 		const token = req.headers.authorization;
 
@@ -26,6 +27,11 @@ const auth = () => {
 
 				// decoded undefined
 				req.user = decoded as JwtPayload;
+
+				const role = (decoded as JwtPayload).role;
+				if (requiredRoles && !requiredRoles.includes(role)) {
+					throw new AppError(status.UNAUTHORIZED, 'You are not authorized!');
+				}
 				next();
 			},
 		);
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,23 +4,28 @@ import { studentValidations } from '../student/student.validation';
 import validateRequest from '../../Middleware/validateRequest';
 import { FacultyValidation } from '../faculty/faculty.validation';
 import { adminValidation } from '../admin/admin.validation';
+import auth from '../../Middleware/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
 router.post(
 	'/create-student',
+	auth(USER_ROLE.admin),
 	validateRequest(studentValidations.createStudentValidationSchema),
 	UserControllers.createStudent,
 );
 
 router.post(
 	'/create-faculty',
+	auth(USER_ROLE.admin),
 	validateRequest(FacultyValidation.createFacultyValidationSchema),
 	UserControllers.createFaculty,
 );
 
 router.post(
 	'/create-admin',
+	// auth(USER_ROLE.admin),
 	validateRequest(adminValidation.createAdminValidationSchema),
 	UserControllers.createAdmin,
 );
