e9e4b586d8d81ee0aca315ece44bdd38287c7e0a
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-07-31 23:33:10 +0600

    Semester Registration more secure

--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { SemesterRegistrationController } from './semesterRegistration.controller';
 import validateRequest from '../../Middleware/validateRequest';
 import { semesterRegistrationValidation } from './semesterRegistration.validation';
+import auth from '../../Middleware/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
 	'/create-semester-registration',
+	auth(USER_ROLE.admin, USER_ROLE.superAdmin),
 	validateRequest(
 		semesterRegistrationValidation.createSemesterRegistrationSchema,
 	),
@@ -15,11 +18,18 @@ router.post(
 
 router.get(
 	'/:id',
+	auth(
+		USER_ROLE.admin,
+		USER_ROLE.faculty,
+		USER_ROLE.student,
+		USER_ROLE.superAdmin,
+	),
 	SemesterRegistrationController.getSingleSemesterRegistration,
 );
 
 router.patch(
 	'/:id',
+	auth(USER_ROLE.admin, USER_ROLE.superAdmin),
 	validateRequest(
 		semesterRegistrationValidation.updateSemesterRegistrationSchema,
 	),
@@ -28,14 +38,30 @@ router.patch(
 
 router.get(
 	'/:id',
+	auth(
+		USER_ROLE.admin,
+		USER_ROLE.faculty,
+		USER_ROLE.student,
+		USER_ROLE.superAdmin,
+	),
 	SemesterRegistrationController.getSingleSemesterRegistration,
 );
 
 router.delete(
 	'/:id',
+	auth(USER_ROLE.admin, USER_ROLE.superAdmin),
 	SemesterRegistrationController.deleteSemesterRegistration,
 );
 
-router.get('/', SemesterRegistrationController.getAllSemesterRegistrations);
+router.get(
+	'/',
+	auth(
+		USER_ROLE.admin,
+		USER_ROLE.faculty,
+		USER_ROLE.student,
+		USER_ROLE.superAdmin,
+	),
+	SemesterRegistrationController.getAllSemesterRegistrations,
+);
 
 export const semesterRegistrationRoutes = router;
